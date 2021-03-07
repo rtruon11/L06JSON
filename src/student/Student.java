a33dbb7660fb14d0f5f759a15e70e293201820cd
Richard <rtruon1@gmail.com>
2021-03-06 20:11:16 -0500

    Changes

--- src/student/Student.java
+++ src/student/Student.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -14,6 +13,9 @@ import org.json.simple.parser.ParseException;
 /**
  *
  * @author Richard Truong
+ * 
+ * Reference: https://howtodoinjava.com/java/library/json-simple-read-write-json-examples/
+ *
  */
 
 public class Student {
@@ -21,18 +23,18 @@ public class Student {
     public static void main(String[] args) {
         objToJSON();
         //displayJSON();
+        System.out.println("");
+        System.out.println("Output: ");
         JSONParser jsonParser = new JSONParser();
          
         try (FileReader reader = new FileReader("Student.json"))
         {
-            //Read JSON file
             Object obj = jsonParser.parse(reader);
  
-            JSONArray employeeList = (JSONArray) obj;
-            System.out.println(employeeList);
+            JSONArray studentList = (JSONArray) obj;
+            System.out.println("Student.json: " + studentList);
              
-            //Iterate over employee array
-            employeeList.forEach( emp -> displayJSON( (JSONObject) emp ) );
+            studentList.forEach( emp -> displayJSON( (JSONObject) emp ) );
  
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -55,28 +57,24 @@ public class Student {
     public void setIName(String IName) { 
       this.IName = IName; 
     }
-    
     public double getGPA() { 
       return GPA; 
     }
     public void setGPA(double GPA) { 
       this.GPA = GPA; 
     }
-    
     public int getCurrentCredits() { 
       return currentCredits; 
     }
     public void setCurrentCredits(int currentCredits) { 
       this.currentCredits = currentCredits; 
     }
-    
     public int getTotalCredits() { 
       return totalCredits; 
     }
     public void setTotalCredits(int totalCredits) { 
       this.totalCredits = totalCredits; 
     }
-    
     public String getfName() { 
       return fName; 
     }
@@ -85,8 +83,6 @@ public class Student {
     }
     
     public static void objToJSON() {
-        
-        //Video Lecture Example
         Scanner scString = new Scanner(System.in);
         Scanner scDouble = new Scanner(System.in);
         Scanner scInteger = new Scanner(System.in);
@@ -94,16 +90,12 @@ public class Student {
         
         System.out.println("Enter IName");
         String IName = scString.nextLine();
-        
         System.out.println("Enter GPA");
         double GPA = scDouble.nextDouble();
-        
         System.out.println("Enter currentCredits");
         int currentCredits = scInteger.nextInt();
-        
         System.out.println("Enter totalCredits");
         int totalCredits = scInteger.nextInt();
-        
         System.out.println("Enter fName");
         String fName = scfName.nextLine();
         
@@ -114,59 +106,24 @@ public class Student {
         students.setTotalCredits(totalCredits);
         students.setIName(fName);
         
-        ArrayList studentsList = new ArrayList(); 
-        studentsList.add(IName);
-        studentsList.add(GPA);
-        studentsList.add(currentCredits);
-        studentsList.add(totalCredits);
-        studentsList.add(fName);
-        
         JSONObject JSONdetails = new JSONObject();
         JSONdetails.put("IName", students.getIName());
         JSONdetails.put("GPA", students.getGPA());
         JSONdetails.put("currentCredits", students.getCurrentCredits());
         JSONdetails.put("totalCredits", students.getTotalCredits());
         JSONdetails.put("fName", students.getfName());
-        //break;
 
         JSONObject JSONStudent = new JSONObject();
         JSONStudent.put("Student", JSONdetails);
 
-        System.out.println(JSONStudent);
-
         JSONArray studentList = new JSONArray();
         studentList.add(JSONStudent);
         
-        System.out.println(studentList);
+        System.out.println("JSON: " + studentList);
         
         try (FileWriter file = new FileWriter("Student.json")) {
             file.write(studentList.toJSONString());
             file.flush();
-            
-        /*
-        //Textbook Example
-        JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder(); 
-        
-            //for (Student student : studentsList) { 
-                jsonArrayBuilder.add( 
-                  Json.createObjectBuilder() 
-                  .add("IName", students.getIName()) 
-                  .add("GPA", students.getGPA()) 
-                  .add("CurrentCredits", students.getCurrentCredits()) 
-                  .add("TotalCredits", students.getTotalCredits()) 
-                  .add("fName", students.getfName())
-                );
-            //}
-
-            JsonArray studentsArray = jsonArrayBuilder.build(); 
-            OutputStream outputStream = new FileOutputStream ("Students.json"); 
-            JsonWriter jsonWriter = Json.createWriter(outputStream); 
-            jsonWriter.writeArray(studentsArray); 
-            System.out.println(studentsArray);
-
-            outputStream.close(); 
-            jsonWriter.close(); 
-        */
         }
         catch (Exception ex) {
             System.out.println("Exception: " + ex);
@@ -174,31 +131,26 @@ public class Student {
     }
     
     public static void displayJSON(JSONObject Student) {
-        /*
         try {
-        InputStream jsonInput = new FileInputStream("Student.json");
-        String json = jsonInput.toString();
-        
-        JSONParser parser = new JSONParser();
-        JSONObject obj = (JSONObject)parser.parse(json);
-        
-        System.out.println("IName: " + obj.get("IName"));
-        System.out.println("GPA: " + obj.get("GPA"));
-        System.out.println("Current Credits: " + obj.get("CurrentCredits"));
-        System.out.println("Total Credits: " + obj.get("TotalCredits"));
-        System.out.println("fName: " + obj.get("fName"));
-        }
-        catch (Exception ex) {
-            System.out.println("Parse Exception: " + ex);
-        }
-        */
-        
-        //Get employee object within list
         JSONObject studentObject = (JSONObject) Student.get("Student");
          
-        //Get employee first name
         String IName = (String) studentObject.get("IName");    
-        System.out.println(IName);
+        System.out.println("IName: " + IName);
         
+        double GPA = (double) studentObject.get("GPA");    
+        System.out.println("GPA: " + GPA);
+        
+        long currentCredits = (long) studentObject.get("currentCredits");    
+        System.out.println("Current Credits: " + currentCredits);
+        
+        long totalCredits = (long) studentObject.get("totalCredits");    
+        System.out.println("Total Credits: " + totalCredits);
+        
+        String fName = (String) studentObject.get("fName");    
+        System.out.println("fName: " + fName);
+        }
+        catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
     }
 }
